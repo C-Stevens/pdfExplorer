15b62519d90e684621b1b42e6c70b312438c4a46
Colin Stevens <mail@colinjstevens.com>
2015-12-30 02:07:19 -0600

    Scrubbed license template

--- src/cjsd32pdfExplorer/Notifiable.java
+++ src/cjsd32pdfExplorer/Notifiable.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cjsd32pdfExplorer;
 
 import java.util.HashMap;
 
 /**
- *
- * @author dale
+ * @author Colin Stevens 
  */
 public interface Notifiable {
     public void notify(String notification);
