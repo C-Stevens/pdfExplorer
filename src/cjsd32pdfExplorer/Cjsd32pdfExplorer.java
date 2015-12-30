3659df01b424e61337f672204b893a7a39d296ec
Colin Stevens <mail@colinjstevens.com>
2015-12-30 02:05:58 -0600

    Scrubbed license template

--- src/cjsd32pdfExplorer/Cjsd32pdfExplorer.java
+++ src/cjsd32pdfExplorer/Cjsd32pdfExplorer.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cjsd32pdfExplorer;
 
 import javafx.application.Application;
@@ -12,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
- *
  * @author Colin Stevens
  */
 public class Cjsd32pdfExplorer extends Application {
